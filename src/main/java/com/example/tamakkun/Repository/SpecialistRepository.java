8beef0b2d870883d00b329e284828bf9070a2cf1
mohammedharbi <mohammedsfh77@gmail.com>
2025-01-03 18:25:10 +0300

    Changes by Durra

--- src/main/java/com/example/tamakkun/Repository/SpecialistRepository.java
+++ src/main/java/com/example/tamakkun/Repository/SpecialistRepository.java
@@ -2,6 +2,7 @@ package com.example.tamakkun.Repository;
 
 import com.example.tamakkun.Model.Specialist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,12 @@ public interface SpecialistRepository extends JpaRepository<Specialist, Integer>
 
     Specialist findSpecialistById(Integer id);
     List<Specialist> findBySupportedDisabilitiesContaining(String disabilityType);
+
+
+    @Query("SELECT s, AVG(r.ratingSpecialist) as avgRating " +
+            "FROM Specialist s JOIN s.reviews r " +
+            "GROUP BY s " +
+            "ORDER BY avgRating DESC")
+    List<Specialist> findTopRatedSpecialistsWithRatings();
 
 }
